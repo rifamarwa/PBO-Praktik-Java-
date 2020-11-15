9ab7e647493968ef54fb287402b55a424defd0d4
rifamarwa <54470387+rifamarwa@users.noreply.github.com>
2020-11-15 20:43:15 +0700

    Add files via upload

---
+++ The_Constructor_with_No_Arguments/Employee.java
@@ -0,0 +1,10 @@
+package The_Constructor_with_No_Arguments;
+
+public class Employee {
+    public String name = "";
+    int salary = 0;
+
+    void show(){
+        System.out.println(name + salary);
+    }
+}
---
+++ The_Constructor_with_No_Arguments/Main.java
@@ -0,0 +1,8 @@
+package The_Constructor_with_No_Arguments;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee emp1 = new Employee();
+        emp1.show();
+    }
+}
