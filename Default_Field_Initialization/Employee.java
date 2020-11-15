44f43d60106ef83e40cf54b285a53e9a0a8fd1ad
rifamarwa <54470387+rifamarwa@users.noreply.github.com>
2020-11-15 18:26:11 +0700

    Add files via upload

---
+++ Default_Field_Initialization/Employee.java
@@ -0,0 +1,11 @@
+package Default_Field_Initialization;
+
+public class Employee {
+    public String nama;
+    public boolean wanita;
+    public int id;
+
+    void show(){
+        System.out.println(this.nama + "\n" + this.id + "\n" + this.wanita);
+    }
+}
---
+++ Default_Field_Initialization/Main.java
@@ -0,0 +1,9 @@
+package Default_Field_Initialization;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee ke1 = new Employee();
+        ke1.show();
+    }
+
+}
