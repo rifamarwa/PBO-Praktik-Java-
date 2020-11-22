de6afc442f4b95745c7b681e854cf1331d8a76d1
rifamarwa <54470387+rifamarwa@users.noreply.github.com>
2020-11-22 10:23:11 +0700

    Add files via upload

---
+++ Class_Comments.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Eksplorasi;
+
+/**
+ *
+ * @author Acer
+ */
+public class Class_Comments {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+
+/**
+ *  A {@code Card} object represents a playing card, such
+ * as "Queen of Hearts". A card has a suit (Diamond, Heart,
+ * Spade or Club) and a value (1 = Ace, 2 . . . 10, 11 = Jack,
+ * 12 = Queen, 13 = King)
+*/
+
+class Card{
+    
+}
