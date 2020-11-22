a7c7622dde77e0cda421733b14c04b81078985b7
rifamarwa <54470387+rifamarwa@users.noreply.github.com>
2020-11-22 10:55:26 +0700

    Add files via upload

---
+++ General_Comments.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Eksplorasi;
+
+/**
+ *
+ * @author Rifa
+ * @version 1.2.1
+ */
+public class General_Comments {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
