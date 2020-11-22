751da8a5f716c8dd754dd1440bd56919daa19b96
rifamarwa <54470387+rifamarwa@users.noreply.github.com>
2020-11-22 09:33:27 +0700

    Add files via upload

---
+++ Static_Importation.java
@@ -0,0 +1,9 @@
+package Eksplorasi;
+import static java.lang.System.*;
+public class Static_Importation {
+    public static void main(String[] args) {
+        out.println("Selamat tinggal..."); //System.out
+        exit(0); //System.exit;
+    }
+    
+}
