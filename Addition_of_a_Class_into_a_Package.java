de425a71ca0f60853d9d89ce48ee8d917680ca44
rifamarwa <54470387+rifamarwa@users.noreply.github.com>
2020-11-22 09:39:05 +0700

    Add files via upload

---
+++ Addition_of_a_Class_into_a_Package.java
@@ -0,0 +1,9 @@
+package Eksplorasi; /*Menempatkan nama package di
+                    paling atas*/
+public class Addition_of_a_Class_into_a_Package {
+
+    public static void main(String[] args) {
+        System.out.println("Sayonara...");
+    }
+    
+}
